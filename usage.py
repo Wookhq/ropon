7dbaebc5320611082b3e575b2e01e2a4de67b768
chip <itsjustchipoverhere@gmail.com>
2025-08-28 22:00:07 +0700

    usage

--- usage.py
+++ usage.py
@@ -4,6 +4,7 @@ from ropon.apis.players.avatar import PlayerOutfit
 from ropon.apis.thumbnails.avatar import RenderAvatar
 from ropon.apis.players.inventory import Inventory
 import json
+import os
 import random
 from dotenv import load_dotenv
 
@@ -17,7 +18,7 @@ inv = Inventory()
 
 # Fetch data with auth_token
 user_id = 3935821483
-auth_token = ""  
+auth_token = os.getenv("ROBLOXTOKEN")
 
 player_info = pl.get_player_info(user_id, auth_token=auth_token)
 games_info = pg.get_games_info(user_id, auth_token=auth_token)
@@ -25,6 +26,7 @@ wearing = po.currently_wearing(user_id, auth_token=auth_token)
 outfits = po.all_outfit(user_id,auth_token=auth_token)
 gamepasses = inv.get_user_gamepass(user_id, count=10 ,auth_token=auth_token)
 badges = inv.get_user_badges(user_id, count=10, auth_token=auth_token)
+decals = inv.get_user_item(user_id, 13, count=10, auth_token=auth_token)
 
 
 print("Player Info:", json.dumps(player_info, indent=2))
