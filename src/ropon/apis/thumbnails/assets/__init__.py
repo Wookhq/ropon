e5864b651beda933f053c2758cfa5c24b8a30166
chip <itsjustchipoverhere@gmail.com>
2025-08-29 11:14:36 +0700

    fix typo

--- src/ropon/apis/thumbnails/assets/__init__.py
+++ src/ropon/apis/thumbnails/assets/__init__.py
@@ -7,12 +7,12 @@ class RenderAsset:
     def __init__(self):
         self.auth_token = None
     @with_auth_token
-    def render_assets(self, item_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"], formanttype: Literal["Png", "Jpeg", "Webp"],**kwargs):
+    def render_assets(self, item_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"], formattype: Literal["Png", "Jpeg", "Webp"],**kwargs):
         try:
             if thumbnail_size not in ["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"]:
                 raise ValueError("thumbnail_size must be 48x48, 50x50, 60x60, 75x75, 100x100, 150x150, 180x180, 352x352, 420x420, 720x720")
-            if formanttype not in ["Png", "Jpeg", "Webp"]:
-                raise ValueError("formanttype must be png, jpeg, webp")
+            if formattype not in ["Png", "Jpeg", "Webp"]:
+                raise ValueError("formattype must be png, jpeg, webp")
             if item_id is None:
                 raise ValueError("item_id cannot be None")
             if not isinstance(item_id, int):
@@ -21,7 +21,7 @@ class RenderAsset:
 
             headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
             req = requests.get(
-                f"https://thumbnails.roblox.com/v1/assets?assetIds={item_id}&returnPolicy=PlaceHolder&size={thumbnail_size}&format=Png&isCircular=false",
+                f"https://thumbnails.roblox.com/v1/assets?assetIds={item_id}&returnPolicy=PlaceHolder&size={formattype}&format=Png&isCircular=false",
                 headers=headers
             )
             req.raise_for_status()
--- src/ropon/apis/thumbnails/avatar/__init__.py
+++ src/ropon/apis/thumbnails/avatar/__init__.py
@@ -7,12 +7,12 @@ class RenderAvatar:
     def __init__(self):
         self.auth_token = None
     @with_auth_token
-    def render_headshot(self, user_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"], formanttype: Literal["Png", "Jpeg", "Webp"], **kwargs):
+    def render_headshot(self, user_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"], formattype: Literal["Png", "Jpeg", "Webp"], **kwargs):
         try:
             if thumbnail_size not in ["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420", "720x720"]:
                 raise ValueError("thumbnail_size must be 48x48, 50x50, 60x60, 75x75, 100x100, 150x150, 180x180, 352x352, 420x420, 720x720")
-            if formanttype not in ["Png", "Jpeg", "Webp"]:
-                raise ValueError("formanttype must be png, jpeg, webp")
+            if formattype not in ["Png", "Jpeg", "Webp"]:
+                raise ValueError("formattype must be png, jpeg, webp")
             if user_id is None:
                 raise ValueError("user_id cannot be None")
             if not isinstance(user_id, int):
@@ -20,7 +20,7 @@ class RenderAvatar:
             
             headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
             req = requests.get(
-                f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size={thumbnail_size}&format=Png&isCircular=false",
+                f"https://thumbnails.roblox.com/v1/users/avatar-headshot?userIds={user_id}&size={thumbnail_size}&format={formattype}&isCircular=false",
                 headers=headers
             )
             req.raise_for_status()
@@ -32,12 +32,12 @@ class RenderAvatar:
             return {"error": f"Invalid input or response: {str(e)}"}
     
     @with_auth_token
-    def render_headshot_bust(self, user_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420"], formanttype: Literal["Png", "Jpeg", "Webp"], **kwargs):
+    def render_headshot_bust(self, user_id: int | None, thumbnail_size: Literal["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420"], formattype: Literal["Png", "Jpeg", "Webp"], **kwargs):
         try:
             if thumbnail_size not in ["48x48", "50x50", "60x60", "75x75", "100x100", "150x150", "180x180", "352x352", "420x420"]:
                 raise ValueError("thumbnail_size must be 48x48, 50x50, 60x60, 75x75, 100x100, 150x150, 180x180, 352x352, 420x420")
-            if formanttype not in ["Png", "Jpeg", "Webp"]:
-                raise ValueError("formanttype must be png, jpeg, webp")
+            if formattype not in ["Png", "Jpeg", "Webp"]:
+                raise ValueError("formattype must be png, jpeg, webp")
             if user_id is None:
                 raise ValueError("user_id cannot be None")
             if not isinstance(user_id, int):
@@ -45,7 +45,7 @@ class RenderAvatar:
             
             headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
             req = requests.get(
-                f"https://thumbnails.roblox.com/v1/users/avatar-bust?userIds={user_id}&size={thumbnail_size}&format={formanttype}&isCircular=false",
+                f"https://thumbnails.roblox.com/v1/users/avatar-bust?userIds={user_id}&size={thumbnail_size}&format={formattype}&isCircular=false",
                 headers=headers
             )
             req.raise_for_status()
@@ -57,12 +57,12 @@ class RenderAvatar:
             return {"error": f"Invalid input or response: {str(e)}"}
     
     @with_auth_token
-    def render_outfit(self, outfit_id: int | None, thumbnail_size: Literal["150x150", "450x450"], formanttype: Literal["Png", "Jpeg", "Webp"], **kwargs):
+    def render_outfit(self, outfit_id: int | None, thumbnail_size: Literal["150x150", "450x450"], formattype: Literal["Png", "Jpeg", "Webp"], **kwargs):
         try:
             if thumbnail_size not in ["150x150", "450x450"]:
                 raise ValueError("thumbnail_size must be '150x150' or '450x450'")
-            if formanttype not in ["Png", "Jpeg", "Webp"]:
-                raise ValueError("formanttype must be png, jpeg, webp")
+            if formattype not in ["Png", "Jpeg", "Webp"]:
+                raise ValueError("formattype must be png, jpeg, webp")
             if outfit_id is None:
                 raise ValueError("outfit_id cannot be None")
             if not isinstance(outfit_id, int):
@@ -70,7 +70,7 @@ class RenderAvatar:
             
             headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
             req = requests.get(
-                f"https://thumbnails.roblox.com/v1/users/outfits?useroutfitIds={outfit_id}&size={thumbnail_size}&format={formanttype}&isCircular=false",
+                f"https://thumbnails.roblox.com/v1/users/outfits?useroutfitIds={outfit_id}&size={thumbnail_size}&format={formattype}&isCircular=false",
                 headers=headers
             )
             req.raise_for_status()
