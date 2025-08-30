f4faf631b4b42e10e85366ca764358ab6341e6c9
chip <itsjustchipoverhere@gmail.com>
2025-08-30 15:05:36 +0700

    group member

--- src/ropon/apis/groups/info/__init__.py
+++ src/ropon/apis/groups/info/__init__.py
@@ -50,3 +50,29 @@ class GroupInfo:
             return {"error": f"Failed to fetch group : {e}"}
         except (KeyError, IndexError, ValueError) as e:
             return {"error": f"Invalid input or response: {str(e)}"}
+    
+    @with_auth_token
+    def get_group_members(self, group_id: int | None, cursor : str | None, **kwargs):
+        try:
+            if group_id is None:
+                raise ValueError("group_id cannot be None")
+            if not isinstance(universe_id, int):
+                raise ValueError("group_id must be a interger")
+            else:
+                cursorsearch = f"&cursor={cursor}"
+
+            
+
+            headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
+            req = requests.get(
+                f"https://groups.roblox.com/v1/groups/{group_id}/users?limit={count}&sortOrder=Asc{cursorsearch}",
+                headers=headers
+            )
+            req.raise_for_status()
+            response = req.json()
+
+            return response
+        except requests.RequestException as e:
+            return {"error": f"Failed to fetch group : {e}"}
+        except (KeyError, IndexError, ValueError) as e:
+            return {"error": f"Invalid input or response: {str(e)}"}
