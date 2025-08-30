12eb3bb6b1f4dbd302338ecd8ef69c084469502e
chip <itsjustchipoverhere@gmail.com>
2025-08-30 11:16:15 +0700

    fav api

--- src/ropon/apis/game/info/__init__.py
+++ src/ropon/apis/game/info/__init__.py
@@ -56,3 +56,26 @@ class GameInfo:
             return {"error": f"Failed to fetch gameinfo : {e}"}
         except (KeyError, IndexError, ValueError) as e:
             return {"error": f"Invalid input or response: {str(e)}"}
+    
+    @with_auth_token
+    def get_fav_info(self, universe_id: int | None, **kwargs):
+        try:
+            if universe_id is None:
+                raise ValueError("universe_id cannot be None")
+            if not isinstance(universe_id, int):
+                raise ValueError("universe_id must be a interger")
+
+            
+            headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
+            req = requests.get(
+                f"https://games.roblox.com/v1/games/{universe_id}/favorites/count",
+                headers=headers
+            )
+            req.raise_for_status()
+            response = req.json()
+
+            return response
+        except requests.RequestException as e:
+            return {"error": f"Failed to fetch gameinfo : {e}"}
+        except (KeyError, IndexError, ValueError) as e:
+            return {"error": f"Invalid input or response: {str(e)}"}
