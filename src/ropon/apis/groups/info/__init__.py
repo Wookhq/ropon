c61462a0379a20442d4c0e8022481938f5dad4da
debian chip <itsjustchipoverhere@proton.me>
2025-09-08 07:31:05 -0400

    add social links

--- src/ropon/apis/groups/info/__init__.py
+++ src/ropon/apis/groups/info/__init__.py
@@ -61,11 +61,31 @@ class GroupInfo:
             else:
                 cursorsearch = f"&cursor={cursor}"
 
-            
-
             headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
             req = requests.get(
                 f"https://groups.roblox.com/v1/groups/{group_id}/users?limit={count}&sortOrder=Asc{cursorsearch}",
+                headers=headers
+            )
+            req.raise_for_status()
+            response = req.json()
+
+            return response
+        except requests.RequestException as e:
+            return {"error": f"Failed to fetch group : {e}"}
+        except (KeyError, IndexError, ValueError) as e:
+            return {"error": f"Invalid input or response: {str(e)}"}
+    
+    @with_auth_token
+    def get_group_social_links(self, group_id: int | None, **kwargs):
+        try:
+            if group_id is None:
+                raise ValueError("group_id cannot be None")
+            if not isinstance(group_id, int):
+                raise ValueError("group_id must be a interger")          
+
+            headers = {".ROBLOSECURITY": self.auth_token} if self.auth_token else {}
+            req = requests.get(
+                f"https://groups.roblox.com/v1/groups/{group_id}/social-links",
                 headers=headers
             )
             req.raise_for_status()
